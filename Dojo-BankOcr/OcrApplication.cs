6999705d5ab75aed8aad928a84cedd9854153120
claus18 <44498648+claus18@users.noreply.github.com>
2022-03-02 14:20:50 +0100

    First try

--- Dojo-BankOcr/OcrApplication.cs
+++ Dojo-BankOcr/OcrApplication.cs
@@ -9,8 +9,23 @@ namespace Dojo_BankOcr
     {
         public string ConvertText(string[] theFile)
         {
-            return null;
+            var line1 = theFile[0];
+            var line2 = theFile[1];
+            var line3 = theFile[2];
+
+            var digit1 = line1.Substring(0, 3) + line2.Substring(0, 3) + line3.Substring(0, 3); 
+            if (digit1 == DigitString.zero)
+            {
+                return "0";
+            }          
+
         }
+
+    }
+
+    public class DigitString{
+        public const string zero = " _ | ||_|";
+
 
     }
 }
